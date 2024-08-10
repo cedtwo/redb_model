b52fadc1624c07a15e2e0eee3a50c33d1b32d694
Christopher Edward Dawn <cedtwo@tutanota.com>
2024-08-10 18:42:10 +0800

    Refactor trait for key/value references

--- redb_model_trait/src/lib.rs
+++ redb_model_trait/src/lib.rs
@@ -1,29 +1,24 @@
 /// Defines a table, and provides helper functions for table entries.
 pub trait Model<'a, K: redb::Key + 'static, V: redb::Value + 'static>: Sized + 'a {
-    /// A key type reference, or a tuple of key type references.
-    type KeyRef;
-    /// A value type reference, or a tuple of value type references.
-    type ValueRef;
+    /// The key type(s) held by an instance of the `Model` type.
+    type ModelKey;
+    /// The value type(s) held by an instance of the `Model` type.
+    type ModelValue;
 
     /// The table definition.
     const DEFINITION: redb::TableDefinition<'a, K, V>;
 
-    /// Return a reference to the `(Key, Value)` pair, wrapped in a tuple for
-    /// composite variables.
-    fn as_values(&'a self) -> (Self::KeyRef, Self::ValueRef);
+    /// Return a reference to the `(Key, Value)` pair.
+    fn as_values(
+        &'a self,
+    ) -> (
+        <K as redb::Value>::SelfType<'a>,
+        <V as redb::Value>::SelfType<'a>,
+    );
 
     /// Consume the entry, returning a `(Key, Value)` pair.
-    fn into_values(self) -> (K, V);
+    fn into_values(self) -> (Self::ModelKey, Self::ModelValue);
 
     /// Instantiate from a `(Key, Value)` pair.
-    fn from_values(values: (K, V)) -> Self;
-
-    /// Instantiate from an `(AccessGuard<K>, AccessGuard<V>)` pair.
-    fn from_guards(values: (redb::AccessGuard<'a, K>, redb::AccessGuard<'a, V>)) -> Self
-    where
-        K: for<'c> redb::Value<SelfType<'c> = K>,
-        V: for<'c> redb::Value<SelfType<'c> = V>,
-    {
-        Self::from_values((values.0.value(), values.1.value()))
-    }
+    fn from_values(values: (Self::ModelKey, Self::ModelValue)) -> Self;
 }
