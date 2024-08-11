8cfeacf84c4cd5e0dac3d62ba26013c3ed60cdb0
Christopher Edward Dawn <cedtwo@tutanota.com>
2024-08-11 15:48:38 +0800

    Fix primitive number types and add from_guards back

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,54 @@
 //! conversion methods/implementations.
 //!
 //! ## Functionality
-//! All functionality is implemented by the [`Model`] trait within. Decorating
-//! a struct with `#[derive(Model)]` will define a `redb::TableDefinition` as
-//! an associated constant for the type, with the specified fields as key/value
-//! types, or tuples of types.
+//!
+//! This crate aims to unify database entry DTO definitions and `redb::TableDefinition`.
+//! Decorating a struct with `#[derive(Model)]` will implement [`Model`] for the type,
+//! define `redb::TableDefinition` as an associated constant, and generating helper
+//! methods for the type.
+//!
+//! ### Example
+//!
+//! ```
+//! # use redb::backends::InMemoryBackend;
+//! # use redb::Database;
+//! # use redb_model::Model;
+//! let db = Database::builder()
+//!     .create_with_backend(InMemoryBackend::new())
+//!     .unwrap();
+//!
+//! #[derive(Model)]
+//! struct User {
+//!     #[entry(position(key))]
+//!     id: u32,
+//!     #[entry(position(value))]
+//!     name: String,
+//!     #[entry(position(value))]
+//!     email: String,
+//! }
+//!
+//! let user = User {
+//!     id: 0,
+//!     name: "User".to_owned(),
+//!     email: "user_email@email.com".to_owned(),
+//! };
+//!
+//! let txn = db.begin_write().unwrap();
+//! {
+//!     let mut table = txn.open_table(User::DEFINITION).unwrap();
+//!     let (k, v) = user.as_values();
+//!     table.insert(k, v).unwrap();
+//! }
+//! txn.commit().unwrap();
+//! ```
 //!
 //! ### Specifying keys and values
 //! Key(s) and value(s) are specified by decorating table fields with
 //! `#[entry(position(...))]`, passing either `key` or `value` to the inner field.
+//! A composite key/value will be combined into a tuple.
+//!
 //! ```
 //! # use crate::redb_model::Model;
-//! #
 //! #[derive(Model)]
 //! struct User {
 //!     #[entry(position(key))]
@@ -28,6 +65,11 @@
 //! let user_value = ("my_name".to_string(), "my_email@email.com".to_string());
 //!
 //! let user = User::from_values((user_key, user_value));
+//! let (k, v) = user.into_values();
+//!
+//! // Only the value is a tuple for this model.
+//! assert_eq!(k, [0; 16]);
+//! assert_eq!(v, ("my_name".to_string(), "my_email@email.com".to_string()));
 //! ```
 //!
 //! ### Specifying a table name
@@ -35,9 +77,7 @@
 //! the struct with `#[model(name = "...")]` attribute.
 //! ```
 //! # use redb::TableHandle;
-//! #
 //! # use crate::redb_model::Model;
-//! #
 //! #[derive(Model)]
 //! #[model(name = "user_table")]
 //! struct User {
@@ -53,13 +93,10 @@
 //! ### Type conversion
 //! The generated implementation of the [`Model`] trait provides methods for
 //! instantiating, borrowing and taking the key/value pairs of the model DTO.
-//! See the [`Model`] trait for available methods. By default, `#[derive(Model)]`
-//! will only generate an implementation of the `Model` trait. Decorating the
-//! struct with `#[model(impl_from)]` will implement `From<T>`, mapping `T` to
-//! the `from_values(T)` method.
+//! Decorating the struct with `#[model(impl_from)]` will further implement
+//! `From<T>`, mapping `T` to the `from_values(T)` and `from_guards(T)` methods.
 //! ```
 //! # use crate::redb_model::Model;
-//! #
 //! #[derive(Model)]
 //! #[model(impl_from)]
 //! struct User {
@@ -78,16 +115,41 @@
 //! ```
 //! ## Implementation details
 //!
-//! The following are general notes regardign implementation or usage.
+//! The following are general notes regarding implementation.
+//!
+//! ### `String` Table Definitions
+//!
+//! `redb_model` will replace `String` with `&str` for table definitions. This
+//! is so that composite (tuple) variables can be borrowed and passed to database
+//! handlers without destructuring the DTO. This is not currently possible with
+//! `String`.
+//! ```compile_error
+//! #use redb::backends::InMemoryBackend;
+//! #use redb::Database;
+//! #
+//! # let db = Database::builder()
+//! #     .create_with_backend(InMemoryBackend::new())
+//! #     .unwrap();
+//! #
+//! const TABLE: TableDefinition<(String, String), ()> = TableDefinition::new("table");
+//!
+//! let string_0 = "string_0".to_string();
+//! let string_1 = "string_1".to_string();
+//!
+//! let txn = db.begin_write().unwrap();
+//! let mut table = txn.open_table(TABLE).unwrap();
+//! // This doesn't work.
+//! table.insert((&string_0, &string_1), ());
+//! // Neither does this.
+//! table.insert((string_0.as_str(), string_1.as_str()), ());
+//! ```
 //!
 //! ### Unit type values
 //!
 //! The unit type `()` must be passed if no value is defined.
 //! ```
 //! # use redb::TableHandle;
-//! #
 //! # use crate::redb_model::Model;
-//! #
 //! #[derive(Model)]
 //! #[model(name = "outbound_edge")]
 //! struct Edge {
@@ -97,7 +159,7 @@
 //!     target: [u8; 16],
 //! }
 //! let k = ([0; 16], [1; 16]);
-//! let v = (); // `()` argument must be passed.
+//! let v = ();
 //! let e = Edge::from_values((k, v));
 //! ```
 //!
