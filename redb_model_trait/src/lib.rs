585a8d30767edee0242c8071482e374b78e554fc
Christopher Edward Dawn <cedtwo@tutanota.com>
2024-10-18 14:34:55 +0800

    Update trait for type conversion

--- redb_model_trait/src/lib.rs
+++ redb_model_trait/src/lib.rs
@@ -8,22 +8,27 @@ pub trait Model<'a> {
     const DEFINITION: Self::TableType;
 }
 
-/// Extension types and traits for entries derived from a `Model`.
+/// Conversion methods for a `Model` and the associated keys and values.
 pub trait ModelExt<'a>: Model<'a> + Sized + 'a {
-    /// The `redb` definition key type.
-    type RedbKey: redb::Key + 'static;
-    /// The `redb` definition value type.
-    type RedbValue: redb::Value + 'static;
+    /// The `redb` definition key type(s).
+    type RedbKey: redb::Key;
+    /// The `redb` definition value type(s).
+    type RedbValue: redb::Value;
 
-    /// The owned key type(s) held by an instance of the `Model` type.
+    /// The model key type(s).
     type ModelKey;
-    /// The owned value type(s) held by an instance of the `Model` type.
+    /// The model key type(s).
     type ModelValue;
 
-    /// Instantiate from a `(Key, Value)` pair.
-    fn from_values(values: (Self::ModelKey, Self::ModelValue)) -> Self;
+    /// Instantiate from a `redb` (`K`, `V`) pair.
+    fn from_values(
+        values: (
+            <Self::RedbKey as redb::Value>::SelfType<'a>,
+            <Self::RedbValue as redb::Value>::SelfType<'a>,
+        ),
+    ) -> Self;
 
-    /// Instantiate from an `AccessGuard` pair. Calls `to_owned` on variables.
+    /// Instantiate from a `redb` (`AccessGuard<K>`, `AccessGuard<V>`) pair.
     fn from_guards(
         values: (
             &redb::AccessGuard<'a, Self::RedbKey>,
@@ -31,25 +36,28 @@ pub trait ModelExt<'a>: Model<'a> + Sized + 'a {
         ),
     ) -> Self;
 
-    /// Instantiate from an owned key and an `AccessGuard` value.
+    /// Instantiate from a `redb` (`K`, `AccessGuard<V>`) pair.
     fn from_key_and_guard(
-        values: (Self::ModelKey, &redb::AccessGuard<'a, Self::RedbValue>),
+        values: (
+            <Self::RedbKey as redb::Value>::SelfType<'a>,
+            &redb::AccessGuard<'a, Self::RedbValue>,
+        ),
     ) -> Self;
 
-    /// Return a reference to the `(Key, Value)` pair.
-    fn as_values(
+    /// Get the `redb` `K` value. May copy, clone or borrow
+    /// depending on the definition.
+    fn as_key(&'a self) -> <Self::RedbKey as redb::Value>::SelfType<'a>;
+
+    /// Get the `redb` `V` value. May copy, clone or borrow
+    /// depending on the definition.
+    fn as_value(&'a self) -> <Self::RedbValue as redb::Value>::SelfType<'a>;
+
+    /// Get all variables as a `redb` `(K, V)` pair. May copy, clone or borrow
+    /// depending on the definition.
+    fn as_key_and_value(
         &'a self,
     ) -> (
         <Self::RedbKey as redb::Value>::SelfType<'a>,
         <Self::RedbValue as redb::Value>::SelfType<'a>,
     );
-
-    /// Consume the entry, returning a `(Key, Value)` pair.
-    fn into_values(self) -> (Self::ModelKey, Self::ModelValue);
-
-    /// Clone the inner key.
-    fn clone_key(&self) -> Self::ModelKey;
-
-    /// Get the inner value.
-    fn clone_value(&self) -> Self::ModelValue;
 }
