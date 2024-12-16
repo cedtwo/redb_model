5055b004d5d77e6701882f544b2efc2dacdf315e
Christopher Edward Dawn <cedtwo@tutanota.com>
2024-12-16 14:56:27 +0800

    Fix readme

--- src/lib.rs
+++ src/lib.rs
@@ -123,7 +123,7 @@
 //! Feature | DTO Type | Database Type | From operation | Into operation
 //! ---|---|---|---|---
 //! `uuid` | `Uuid` | `[u8; 16]` | `Uuid::from_bytes(field)` | `field.as_bytes()`
-//! `secrecy` | 'SecretString | `&str` | "SecretString::from(field)" | `ExposeSecret::expose_secret(&field)`
+//! `secrecy` | `SecretString` | `&str` | `SecretString::from(field)` | `ExposeSecret::expose_secret(&field)`
 //! `secrecy` | `SecretBox<S>`  | `S` | `SecretBox::new(Box::new(*field))` | `*ExposeSecret::expose_secret(&field)`
 //!
 //! ```rust
